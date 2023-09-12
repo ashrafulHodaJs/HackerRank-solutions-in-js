94829d60e1ad373999a92fef4d050510be9b27da
Ashraful Hoda <141621146+ashrafulHodaJs@users.noreply.github.com>
2023-09-13 03:01:00 +0530

    Create hrsol.js

---
+++ hrsol.js
@@ -0,0 +1,57 @@
+//Solve Me First
+function solveMeFirst(a, b) {
+  return a+b
+}
+//Simple Array Sum
+function simpleArraySum(ar) {
+   let sum = 0;
+   for(let i=0; i<ar.length; i++){
+      sum = sum + ar[i];
+      ;
+   } 
+return sum
+}
+//Compare the Triplets
+function compareTriplets(a, b) {
+    
+  let aScore = 0;
+  let bScore = 0; 
+  for(let i=0; i<a.length; i++){
+      if(a[i]>b[i]){
+         aScore++ 
+      }else if(a[i]<b[i]){
+          bScore++
+      }
+  }
+ return [aScore, bScore];
+}
+//A Very Big Sum
+function aVeryBigSum(ar) {
+    // Write your code here
+    let sum=0;
+    for(let i=0; i<ar.length; i++){
+        sum+=ar[i]
+    }
+    return sum;
+
+}
+//Plus Minus
+unction plusMinus(arr) {
+   const n =arr.length;
+   let pos = 0;
+   let neg = 0;
+   let zer = 0;
+   for(let i=0; i<n; i++){
+       if(arr[i]==0){
+           zer++;
+       }else if(arr[i]>0){
+           pos++;
+       }else{
+           neg++
+       }
+   }
+   console.log(pos/n);
+   console.log(neg/n);
+   console.log(zer/n);
+
+}
