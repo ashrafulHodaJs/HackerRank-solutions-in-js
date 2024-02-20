0dd46526c673f0b4fded0e107c1bda0407ce633c
Ashraful Hoda <141621146+ashrafulHodaJs@users.noreply.github.com>
2024-02-20 20:18:49 +0530

    Update hrsol.js

--- hrsol.js
+++ hrsol.js
@@ -55,3 +55,31 @@ unction plusMinus(arr) {
    console.log(zer/n);
 
 }
+// camelCase
+function camelcase(s) {
+    // Write your code here
+    let count = 0;
+    for(let i=0; i<s.length; i++){
+        if(s.charCodeAt(i)<97){
+            count++;
+        }
+    }
+    return count+1;
+
+}
+// Angry Professor
+function angryProfessor(k, a) {
+   let count =0;
+   for(let i=0; i<a.length; i++){
+       if(a[i]<=0){
+           count++;
+       }
+   }
+   if(count>=k){
+       return "NO";
+   }else{
+       return "YES";
+   }
+
+}
+
